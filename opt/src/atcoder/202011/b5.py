11999ef402a5521ef99171f208b38e3b28eb1bc7
jacoloves <5511068t@gmail.com>
2020-11-19 23:58:25 +0900

    20201119

--- opt/src/atcoder/202011/b5.py
+++ opt/src/atcoder/202011/b5.py
@@ -1,18 +1,14 @@
 s = input()
 t = input()
 
-maxcnt = 0
-cnt = 0
+ans = len(s)
+
 for i in range(len(s)):
-    if s[i] == t[cnt]:
-        cnt += 1
-    else:
-        if maxcnt < cnt:
-            maxcnt = cnt
-        cnt = 0
+    if i + len(t) - 1 >= len(s): break
+    dif = 0
+    for j in range(len(t)):
+        if s[i+j] != t[j]:
+            dif += 1
+    ans = min(ans, dif)
 
-if maxcnt == 0:
-    if s[len(t)-1] == t[len(t)-1]:
-        maxcnt += 1
-
-print(len(t) - maxcnt)
+print(ans)
