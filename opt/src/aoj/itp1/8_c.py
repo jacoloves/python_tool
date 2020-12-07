f98fa9a92740673c159832a1203a72f6d4985bcc
jacoloves <5511068t@gmail.com>
2020-12-07 23:29:26 +0900

    20201207

---
+++ opt/src/aoj/itp1/8_c.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+text = ''
+
+while True:
+    try:
+        text += input().lower()
+    except EOFError:
+        break
+
+for a in alpha:
+    print('{} : {}'.format(a, text.count(a)))
